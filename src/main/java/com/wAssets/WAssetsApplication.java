7f81d096e6859fd4f061c4d9954ecdd07a1e5b0c
JeHoon <JeHoon@DESKTOP-0NU9ABL>
2020-09-27 21:35:02 +0900

    wAssets 최초 커밋

---
+++ src/main/java/com/wAssets/WAssetsApplication.java
@@ -0,0 +1,13 @@
+package com.wAssets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WAssetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WAssetsApplication.class, args);
+	}
+
+}
