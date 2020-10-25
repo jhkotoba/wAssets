4546e1efbd93fb5cef1a3004ea1bc18a362184e9
JeHoon <JeHoon@DESKTOP-0NU9ABL>
2020-10-25 17:32:46 +0900

    상수 추가

--- src/main/java/com/wAssets/common/Constant.java
+++ src/main/java/com/wAssets/common/Constant.java
@@ -19,6 +19,7 @@ public interface Constant {
 	public static final String CODE_SERVER_ERROR = "9000";
 	public static final String CODE_REPOSITORY_ERROR = "9001";
 	public static final String CODE_INSERT_EMPTY_ERROR = "9001";
+	public static final String CODE_UTIL_CONVERTER_ERROR = "9002";
 	public static final String CODE_UNKNOWN_ERROR = "9444";
 	
 	/* member */
